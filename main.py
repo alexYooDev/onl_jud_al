316b0514f0c9677ea04178234c798b366dbc51f5
Hwanik Alexander Yoo <70088347+alexYooDev@users.noreply.github.com>
2021-06-12 12:24:55 +0000

    solved reversed ternary number

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def solution(a, b):
 
-    answer = sum([n1*n2 for n1,n2 in zip(a,b)])
+def convert(num, base):
+  T = "0123456789ABCDEF"
+  q,r = divmod(num,base)
+  if q == 0:
+    return T[r]
+  else:
+    return convert(q,base)+T[r]
+
+def div(word):
+  return [char for char in word]
+
+def solution(n):
+    answer = 0
+    stk = []
+    three = []
+    result = convert(n, 3)
+    stk = div(str(result))
+    for i in range(0,len(stk)):
+        three += stk.pop()
+    temp = ''.join(three)
+    answer = int(temp, base=3)
     return answer
-
-print(solution([1,2,3,4],[-3,-1,0,2]))
