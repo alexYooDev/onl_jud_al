274dc70bb415d2607efc1f652cec46aac6b1dc7e
Hwanik Alexander Yoo <70088347+alexYooDev@users.noreply.github.com>
2021-06-12 16:20:56 +0000

    solved arith_seq test

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def solution(arr1, arr2):
-    answer = [[]]
-    list2 = arr1.pop()
-    list1 = arr1.pop()
-    list4 = arr2.pop()
-    list3 = arr2.pop()
-    answer = [[x+y for x,y in zip(list1,list3)],[a+b for a,b in zip(list2,list4)]]
+def solution(x, n):
+    answer = [i*x + x for i in range(0,n)]
+     
     return answer
