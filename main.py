9ea7b8bdc335e1bd48cba5dca38c9d693126177a
Hwanik Alexander Yoo <70088347+alexYooDev@users.noreply.github.com>
2021-06-12 15:55:15 +0000

    solved sum matrix test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def solution(n):
-    arr = list(str(n))
-    arr.sort(reverse=True)
-    answer = int("".join(arr))
+def solution(arr1, arr2):
+    answer = [[]]
+    list2 = arr1.pop()
+    list1 = arr1.pop()
+    list4 = arr2.pop()
+    list3 = arr2.pop()
+    answer = [[x+y for x,y in zip(list1,list3)],[a+b for a,b in zip(list2,list4)]]
     return answer
