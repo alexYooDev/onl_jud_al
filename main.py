bd940aeaaf0c64e9c0b5fdb37b0af036b9254383
Hwanik Alexander Yoo <70088347+alexYooDev@users.noreply.github.com>
2021-06-13 00:36:44 +0000

    solved sum of divisor test

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def solution(x, n):
-    answer = [i*x + x for i in range(0,n)]
-     
+def solution(n):
+    answer = 0
+    add = []
+    for i in range(1, n+1):
+        if n%i == 0:
+            add.append(i)
+    answer = sum(add)
+
     return answer
