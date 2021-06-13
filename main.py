8847951e832b5481c6f89333fbc6c38c94dd54b4
Hwanik Alexander Yoo <70088347+alexYooDev@users.noreply.github.com>
2021-06-13 01:32:53 +0000

    new solution

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import re
 
 def solution(s):
-    answer = False
-    if len(s) == 4 or len(s) == 6:
-        answer = True
-
-    if re.findall('[a-z]+', s):
-        answer = False
     
-    return answer
-print(solution("123456"))
+    answer = False
+
+    return s.isdigit() and len(s) in (4, 6)
